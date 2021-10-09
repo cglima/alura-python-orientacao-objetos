bf6887ba69c662d81e77587aff306e4ba457ad90
Cassiana <lima.cglima@gmail.com>
2021-10-09 18:33:49 -0300

    criando getters e setters

--- conta.py
+++ conta.py
@@ -16,6 +16,16 @@ class Conta:
     def saca(self, valor):
         self.__saldo -= valor
 
-    def transfere(self,valor, destino):
+    def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def set_limite(self, limite):
+        self.__limite = limite
+
